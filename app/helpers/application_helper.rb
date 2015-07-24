00b2a55fa5723c4b881b14062b6d874b627fb94f
Karol Moroz <dmuhafc@gmail.com>
2015-07-24 12:44:28 +0200

    Better-looking posts#new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,3 @@
 module ApplicationHelper
-  def do_formatting(str, textile_enabled)
-    if textile_enabled
-      render_textilized(str)
-    else
-      render_wiki_code(str)
-    end
-  end
 
-  def render_textilized(str)
-    raw(RedCloth.new(str).to_html)
-  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,23 @@
 module PostsHelper
+  def do_formatting(str, textile_enabled)
+    if textile_enabled
+      render_textilized(str)
+    else
+      render_wiki_code(str)
+    end
+  end
+
+  def render_textilized(str)
+    raw(RedCloth.new(str).to_html)
+  end
+
+  def render_wiki_code(str)
+    s = str.dup
+    s = str.gsub(/'{3}[^']+'{3}/) { |s| "<strong>#{s[3..-4]}</strong>" }
+    s = s.gsub(/'{2}([^']+)'{2}/) { |s| "<em>#{s[2..-3]}</em>" }
+    s = s.gsub(/\[{2}([\w\d ]+)\]{2}/) { |s| link_to s[2..-3], "/posts/#{s[2..-3]}" }
+    #s = word_wrap(s)
+    s = simple_format(s)
+    return s
+  end
 end
