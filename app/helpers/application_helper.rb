554d5bd3badd854173343a3358eeb0312f3f0220
Karol Moroz <dmuhafc@gmail.com>
2015-08-01 13:10:58 +0200

    More backgrounds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
       strip_tags(string).split('').length
     end
   end
+
+  def background
+    images = ["background-sand.jpg", "background-spain.jpg",
+      "background-night.jpg", "background-xian.jpg", "background-creatures.jpg", "background-tht.jpg", "background-gate.jpg",
+      "background-buddha.jpg", "background-tatooine.jpg"]
+    path = "url(/images/backgrounds/" + images.shuffle.first + ")"
+  end
 end
