7b0cea82f13df94addc4f87479c7283ab188cea4
Karol Moroz <dmuhafc@gmail.com>
2015-08-04 17:14:58 +0200

    Positioning mobile logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
   end
 
   def which_layout
-    if browser.mobile? || browser.tablet?
+    if browser.mobile?
       'mobile'
     else
       'application'
