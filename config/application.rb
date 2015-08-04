b6f337ee41861daaba2dde9eb4abf753bee75cc1
Karol Moroz <dmuhafc@gmail.com>
2015-08-04 16:05:47 +0200

    Localized date

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,6 @@ module ElefantoNew
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.enforce_available_locales = false
     config.i18n.available_locales = [:en, :pl, :zh]
     config.i18n.default_locale = :en
