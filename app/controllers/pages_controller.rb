b1220e45fb7ff34d649ecb6720ed8069aaeae732
Karol Moroz <dmuhafc@gmail.com>
2015-08-05 15:04:12 +0200

    Messing with layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,13 @@
 class PagesController < ApplicationController
   def home
-    @title = I18n.t("homepage")
+    @title = "Elefanto &mdash; " + I18n.t("homepage")
   end
 
   def about
-    @title = I18n.t("about")
+    @title = "Elefanto &mdash; " + I18n.t("about")
   end
 
-  def links
-    @title = I18n.t("links")
+  def faq
+    @title = "Elefanto &mdash; " + I18n.t("faq")
   end
 end
