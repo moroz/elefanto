4352d298482bc4d52831e9800e06e67044c2a362
Karol Moroz <dmuhafc@gmail.com>
2015-09-09 18:19:55 +0200

    Comment form, comment message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,11 +14,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    id = @comment.id
+    @post = @comment.post
+    if @comment.delete
+      flash[:success] = "The comment ##{id} has been deleted successfully."
+      redirect_to @post
+    else
+      flash[:danger] = "The comment could not be deleted."
+      redirect_to post_path(@post, :anchor => "comment_#{id}")
+    end
+  end
+
   def comment_params
     params.require(:comment).permit(:text,:website,:signature,:post_id)
   end
 
   def find_post
-    @post = Post.find(params[:post_id])
+    id = params[:post_id] || params[:id]
+    @post = Post.find(id)
   end
 end
