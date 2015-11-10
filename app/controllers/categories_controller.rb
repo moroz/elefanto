a99b25b8d4f00599ceaebe552f736c14e429f5bd
Karol Moroz <dmuhafc@gmail.com>
2015-11-10 11:03:21 +0100

    Added categories#edit, categories#new and name validation

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,15 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @posts = @category.posts.order(:number => :desc)
   end
+
+  def edit
+    @category = Category.find(params[:id])
+    @header = "Editing category: #{@category.name_en}"
+  end
+
+  def new
+    @category = Category.new
+    @header = "Create a new category"
+    render 'edit'
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,19 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :posts
+
+  validates :name_en, :presence => true, :length => {:minimum => 3}, :uniqueness => true
+  validates :name_pl, :uniqueness => true
+  validates :name_zh, :uniqueness => true
+
+  def name_pl
+    self.name_pl || name_en
+  end
+
+  def name_zh
+    self.name_zh || name_en
+  end
+
+  def name
+    self.name_en
+  end
 end
