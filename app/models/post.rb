476c93e2ac22ae84fcc2dd605622c7bfc733a7fc
Karol Moroz <dmuhafc@gmail.com>
2015-12-12 01:27:38 +0100

    Different counting of characters in Chinese posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -79,7 +79,7 @@ class Post < ActiveRecord::Base
     unless chinese
       ActionController::Base.helpers.strip_tags(string).split.length
     else
-      ActionController::Base.helpers.strip_tags(string).split('').length
+      ActionController::Base.helpers.strip_tags(string).gsub(/[^\p{Han}\w],'').split('').length
     end
   end
 end
