a2ed7ab4d01584711cff045793a72410139f4067
Karol Moroz <dmuhafc@gmail.com>
2016-08-11 12:34:27 +0200

    Add helper for post links

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,13 @@
 module PostsHelper
-  def do_formatting(str, textile_enabled)
+  def do_formatting(str, textile_enabled=true)
     str.gsub!(/<!--(.*?)-->(\n|\r\n|\n\r)/i, '')
     str.gsub!(/<br\s*\/*>(\n|\r\n|\n\r)/, "\n")
     str.gsub!(/\[\[Image\:([\w\-_]+)\]\]/) { |x| image_from_s3($1) }
+    str.gsub!(/\[\[([\d\.t]+)\]\]/) do
+      number = $1.to_i
+      with_title = !!$1.match(/t/)
+      post_link(number, with_title: with_title)
+    end
     if textile_enabled
       render_textilized(str)
     else
@@ -46,5 +51,15 @@ module PostsHelper
       "外"
     end
     content_tag :div, label, :class => css_class
+  end
+
+  def post_link(number, with_title: false)
+    post = Post.find_by(number: number)
+    return "[[#{number}]]" if post.blank?
+    if with_title
+      link_to post.title_with_number, post_path(post)
+    else
+      link_to (post.readable_number + "."), post_path(post)
+    end
   end
 end
--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe PostsHelper do
       expect(helper.language_label("pt")).to match("post__language\">外")
     end
   end
+
+  describe "#post_link" do
+    before(:each) do
+      blog_post = FactoryGirl.build_stubbed(:post, number: 108, title: "Test post")
+      allow(Post).to receive(:find_by).and_return(blog_post)
+    end
+
+    it "displays link to correct post for defined languages" do
+      expect(helper.post_link(108)).to eq("<a href=\"/posts/108-test-post\">108.</a>")
+    end
+
+    describe "#do_formatting" do
+      it "returns post_link when given [[number]]" do
+        expect(helper.do_formatting('[[108]]', true)).to match(helper.post_link(108))
+      end
+      it "returns post_link with title when given [[numbert]]" do
+        expect(helper.do_formatting('[[108t]]', true)).to match(helper.post_link(108, with_title: true))
+      end
+    end
+  end
 end
